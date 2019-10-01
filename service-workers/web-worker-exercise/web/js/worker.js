83e1efec1793e26ed224f9da8545d2491c4c4fd0
Pat Palmerston <patpalmerston@gmail.com>
2019-10-01 15:02:57 -0700

    fibonacci recursion

--- service-workers/web-worker-exercise/web/js/worker.js
+++ service-workers/web-worker-exercise/web/js/worker.js
@@ -1,14 +1,26 @@
-"use strict";
+'use strict';
 
 var curFib = 0;
 
-// TODO
+self.postMessage('Hello from the web worker');
+self.onmessage = onMessage;
 
 // **********************************
+
+function onMessage(evt) {
+	getNextFib();
+}
+
+function getNextFib() {
+	var fibNum = fib(curFib);
+	self.postMessage({ idx: curFib, fib: fibNum });
+	curFib++;
+	setTimeout(getNextFib, 0);
+}
 
 function fib(n) {
 	if (n < 2) {
 		return n;
 	}
-	return fib(n-1) + fib(n-2);
+	return fib(n - 1) + fib(n - 2);
 }
