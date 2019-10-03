e7f531b668faa2cf2e066b02cef2bd763e794d71
Pat Palmerston <patpalmerston@gmail.com>
2019-10-03 13:07:58 -0700

    creating a service worker

--- service-workers/service-worker-exercise/web/js/sw.js
+++ service-workers/service-worker-exercise/web/js/sw.js
@@ -1,3 +1,23 @@
-"use strict";
+'use strict';
 
-// TODO
+const version = 1;
+
+self.addEventListener('install', onInstall);
+self.addEventListener('activate', onActivate);
+
+main().catch(console.error);
+
+// ***************************
+
+async function main() {
+	console.log(`Service Worker (${version}) is starting`);
+}
+
+async function onInstall() {
+	console.log(`Service Worker (${version}) installed`);
+	self.skipWaiting();
+}
+
+async function onActivate() {
+	console.log(`Service Worker (${version}) activated.`);
+}
